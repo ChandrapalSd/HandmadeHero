fcdd7546f50b7d75b9c9205c7a65870f1df65985
chand <chand@DESKTOP-CJ7IQSV>
2022-01-18 20:06:35 +0530

    Add project files.

---
+++ handmade/code/win32_handmade.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int CALLBACK WinMain(  HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+MessageBoxA(0,"This is handmade hero.", "handmade hero", MB_OK | MB_ICONINFORMATION);
+return 0;
+}
